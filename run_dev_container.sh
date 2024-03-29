37db2216774af69bce441a742d87b7996d71626e
Giovanni Aguirre <giovanni.fi05@gmail.com>
2024-03-29 04:49:03 +0000

    Add template files and fix creds filename

--- run_dev_container.sh
+++ run_dev_container.sh
@@ -4,11 +4,11 @@
 
 CCBOT_CERTS_DIR=$(pwd)/certs_le
 CCBOT_ENV_FILE=$(pwd)/.env
-CCBOT_CREDS_DO_FILE=$(pwd)/creds_digitalocean
+CCBOT_CREDS_DO_FILE=$(pwd)/.creds_digitalocean
 
 # Run container
 docker run -it --rm --name ccertbot           \
     -v "${CCBOT_CERTS_DIR}:/etc/letsencrypt"  \
     -v "${CCBOT_ENV_FILE}:/opt/ccertbot/.env" \
-    -v "${CCBOT_CREDS_DO_FILE}:/opt/ccertbot/creds_digitalocean" \
+    -v "${CCBOT_CREDS_DO_FILE}:/opt/ccertbot/.creds_digitalocean" \
     giobyte8/ccertbot:dev
